5057bb20296788337c2ac5cfcfbcdfd1e2891b49
parajulisanket <parajulisanket7@gmail.com>
2023-12-04 21:18:34 +0545

    AA

--- exercise_day2.py
+++ exercise_day2.py
@@ -1,10 +1,10 @@
-first_name = input("enter 1st name: ")
-last_name = input("enter 2nd name: ")
-age = int(input("enter your age: "))
+# first_name = input("enter 1st name: ")
+# last_name = input("enter 2nd name: ")
+# age = int(input("enter your age: "))
 
-#print('Your fullname is '+ first_name.capitalize() + ' ' + last_name.capitalize() + '.')
-#print("your age in 5 years will be " + str(age +5) + '.')
+# #print('Your fullname is '+ first_name.capitalize() + ' ' + last_name.capitalize() + '.')
+# #print("your age in 5 years will be " + str(age +5) + '.')
 
-print(f'hi {first_name.capitalize()} {last_name.capitalize()}') 
-print(f'In 5 years you will be {age +5}')
-print("thank you!!")
+# print(f'hi {first_name.capitalize()} {last_name.capitalize()}') 
+# print(f'In 5 years you will be {age +5}')
+# print("thank you!!")
