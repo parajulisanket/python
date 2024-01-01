b6ce8a87c4ca3ac6075538139a958a19042f8a87
sanketparajuli <parajulisanket7@gmail.com>
2024-01-01 19:46:50 +0545

    module created

--- MODULE & PACKAGES.py
+++ MODULE & PACKAGES.py
@@ -26,5 +26,24 @@
 #------------------------------------------------
 
 
+# import module_1
 
+# print("Additon :",module_1.add(10,20))
+# print("Substraction :",module_1.subtract(10,20))
+# print("Multiplication :",module_1.multiply(10,20))
+# print("Divison :",module_1.division(10,20))
+
+#------------------------
+# import module_1 as m
+# print(m.add(10,20))
+
+#-------------------------
+# from module_1 import division
+# print(division(20,10))
+# from module_1 import multiply
+# print(multiply(20,10))
+# from module_1 import subtract
+# print(subtract(20,10))
+# from module_1 import add
+# print(add(20,10))
 
---
+++ module_1.py
@@ -0,0 +1,18 @@
+def add(x,y):
+    answer = x + y
+    return answer
+
+def subtract(x,y):
+    if y > x:
+        answer = y - x
+    else:
+        answer = x - y
+    return answer
+
+def multiply(x,y):
+    answer = x * y
+    return answer
+
+def division(x,y):
+    answer = x / y
+    return answer
